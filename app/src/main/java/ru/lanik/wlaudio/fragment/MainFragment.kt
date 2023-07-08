f05294a4dd786ce92a7017447141401a0e56a33c
Lanik <daniilt971@gmail.com>
2023-07-08 11:56:48 +0300

    app: src: MainFragment: Init fragment

---
+++ app/src/main/java/ru/lanik/wlaudio/fragment/MainFragment.kt
@@ -0,0 +1,41 @@
+package ru.lanik.wlaudio.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+import ru.lanik.wlaudio.ui.screen.main.MainScreen
+import ru.lanik.wlaudio.ui.screen.main.MainViewModel
+import ru.lanik.wlaudio.ui.screen.main.MainViewModelFactory
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainFragment : Fragment() {
+    @Inject
+    lateinit var viewModelFactory: MainViewModelFactory
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        viewModel = viewModelFactory.getViewModel(
+            navController = findNavController(),
+        )
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MaterialTheme {
+                    MainScreen(
+                        viewModel = viewModel,
+                    )
+                }
+            }
+        }
+    }
+}
