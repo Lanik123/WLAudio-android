f55ef98067b61c9d1edb91f85dbfeaf70a6a1d1b
Lanik <daniilt971@gmail.com>
2023-07-11 22:30:49 +0300

    app: src: MainFragment: Apply settings to theme

--- app/src/main/java/ru/lanik/wlaudio/fragment/MainFragment.kt
+++ app/src/main/java/ru/lanik/wlaudio/fragment/MainFragment.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
+import ru.lanik.wlaudio.data.SettingsManager
 import ru.lanik.wlaudio.ui.screen.main.MainScreen
 import ru.lanik.wlaudio.ui.screen.main.MainViewModel
 import ru.lanik.wlaudio.ui.screen.main.MainViewModelFactory
@@ -22,6 +23,9 @@ class MainFragment : Fragment() {
     lateinit var viewModelFactory: MainViewModelFactory
     private lateinit var viewModel: MainViewModel
 
+    @Inject
+    lateinit var settingsManager: SettingsManager
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -32,12 +36,20 @@ class MainFragment : Fragment() {
         )
         return ComposeView(requireContext()).apply {
             setContent {
-                WLAudioTheme {
+                val settingsState = settingsManager.collectAsState()
+                WLAudioTheme(
+                    textSize = settingsState.value.textSize,
+                    paddingSize = settingsState.value.paddingSize,
+                    corners = settingsState.value.cornerStyle,
+                    darkTheme = settingsState.value.isDark,
+                ) {
                     SetStatusBarColor(
                         color = WLAudioTheme.colors.primaryBackground,
+                        isDarkMode = settingsState.value.isDark,
                     )
                     SetNavigationBarColor(
                         color = WLAudioTheme.colors.primaryBackground,
+                        isDarkMode = settingsState.value.isDark,
                     )
                     MainScreen(
                         viewModel = viewModel,
